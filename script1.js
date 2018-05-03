7c637efe858939ea678dba2183aade11e9dedb57
purna shrestha <purna.stha12315@gmail.com>
2018-05-03 14:27:35 +0545

    helicopter harayo

--- script1.js
+++ script1.js
@@ -33,7 +33,9 @@ function moveLeft(){
 	currentLeft = circle.offsetLeft;
 	circle.style.left = currentLeft - 3 + 'px';
 	circle.style.transition='transform 2s';
-	circle.style.transform='scale(0.2)';
+	circle.style.transform='scale(0.0)';
+	var copacity= parseFloat(circle.style.opacity);
+	circle.style.opacity=copacity-0.005;
 }
 function moveRight(){
 	var circle = document.getElementById('circle');
@@ -41,13 +43,17 @@ function moveRight(){
 	circle.style.left = currentLeft + 3 + 'px';
 	circle.style.transition='transform 2s';
 	circle.style.transform='scale(1.2)';
+	var copacity= parseFloat(circle.style.opacity);
+	circle.style.opacity=copacity+0.010;
 }
 function moveTop(){
 	var circle = document.getElementById('circle');
 	currentTop = circle.offsetTop;
 	circle.style.top = currentTop - 3 + 'px';
 	circle.style.transition='transform 2s';
-	circle.style.transform='scale(0.2)';
+	circle.style.transform='scale(0.0)';
+	var copacity= parseFloat(circle.style.opacity);
+	circle.style.opacity=copacity-0.005;
 
 }
 function moveDown(){
@@ -56,4 +62,6 @@ function moveDown(){
 	circle.style.top = currentTop + 3 + 'px';
 	circle.style.transition='transform 2s';
 	circle.style.transform='scale(1.2)';
+		var copacity= parseFloat(circle.style.opacity);
+	circle.style.opacity=copacity+0.010;
 }
